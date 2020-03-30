48a415d39e5f1d4767a6b568567c184d71947784
xiateserenity <xiateserenity@gmail.com>
2020-03-29 19:50:36 -0500

    headlines pipeline working. TODO: work on react frontend

--- grover.scraper.js
+++ grover.scraper.js
@@ -3,58 +3,60 @@ const newsapi = require('./news.api');
 
 const prompts = [];
 
-(() => {
-  newsapi
-    .getHeadlines()
-    .then(headlines => console.log(headlines))
-    .catch(err => console.log(err.message));
-})();
-
 (async () => {
-  const browser = await pptr.launch();
+  const browser = await pptr.launch({
+    args: ['--disable-dev-shm-usage']
+  });
 
   try {
     const page = await browser.newPage();
+
+    newsapi
+      .getHeadlines()
+      .then(headlines => prompts.push(...headlines))
+      .catch(err => console.log(err.message));
+
     await page.goto('https://grover.allenai.org/');
 
-    const prompt =
-      "'A game changer': FDA authorizes Abbott Labs' portable, 5-minute coronavirus test the size of a toaster";
+    for (let i = 0; i < prompts.length; i++) {
+      await page
+        .evaluate(() => {
+          document.querySelector(
+            '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(6) > input'
+          ).value = '';
+          document.querySelector(
+            '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(12) > textarea'
+          ).value = '';
+          document.querySelector(
+            '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(15) > textarea'
+          ).value = '';
+          document.querySelector(
+            '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(18) > textarea'
+          ).value = '';
+        })
+        .catch(err => console.log(err.message));
 
-    await page.evaluate(() => {
-      document.querySelector(
-        '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(6) > input'
-      ).value = '';
-      document.querySelector(
-        '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(12) > textarea'
-      ).value = '';
-      document.querySelector(
-        '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(15) > textarea'
-      ).value = '';
-      document.querySelector(
-        '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(18) > textarea'
-      ).value = '';
-    });
+      await page.type(
+        '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(15) > textarea',
+        prompts[i]
+      );
 
-    await page.type(
-      '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(15) > textarea',
-      prompt
-    );
+      await page.click(
+        '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(18) > button'
+      );
 
-    await page.click(
-      '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-dNLxif.eotRhV > div:nth-child(18) > button'
-    );
+      console.log('Generating article...');
 
-    console.log('Generating article...');
+      await page.waitFor(60000);
 
-    await page.waitFor(60000);
-
-    console.log(
-      await page.evaluate(() => {
-        return document.querySelector(
-          '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-jqCOkK.hDIgwA > div.sc-feJyhm.fRrBax > div.sc-jtRfpW.eslsTz > span'
-        ).innerHTML;
-      })
-    );
+      console.log(
+        await page.evaluate(() => {
+          return document.querySelector(
+            '#root > section > main > div > div:nth-child(2) > div.sc-kvZOFW.sc-jqCOkK.hDIgwA > div.sc-feJyhm.fRrBax > div.sc-jtRfpW.eslsTz > span'
+          ).innerHTML;
+        })
+      );
+    }
   } catch (err) {
     console.log(err.message);
   } finally {
